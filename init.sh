d4180d2b0087e661ffa931a0179ef57bd6f0b1c5
jerem-33 <38467369+jerem-33@users.noreply.github.com>
2018-04-23 22:20:49 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,1 +1,3 @@
+yum install open-vm-tools 
 yum install wget curl python dmidecode -y
+https://vra01srv01.int.virtualy-labs.com/software/download/prepare_vra_template_linux.tar.gz --no-check-certificate
